21dd2cfc456d783e0a763ce75763b6cd463f42df
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-04 17:44:47 +0700

    init project

---
+++ src/test/java/com/se/backend/ecommerceapp/EcommerceAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.backend.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppBackEndApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}

ab325642fa03fc13bd905e628ccf76cf27f5e271
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-11-08 16:23:28 +0700

    add eureka server project

--- src/main/java/com/se/backend/ecommerceapp/EcommerceAppBackEndApplication.java
+++ src/main/java/com/se/backend/ecommerceapp/EcommerceAppBackEndApplication.java
@@ -2,8 +2,10 @@ package com.se.backend.ecommerceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EcommerceAppBackEndApplication {
     
     public static void main(String[] args) {
